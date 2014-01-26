62d1203e4cbd2fa74b1d917f62f1c327f2c938af
Jens Rapp <rapp.jens@googlemail.com>
2014-01-26 18:43:48 +0100

    alles fertig

--- program.sh
+++ program.sh
@@ -5,8 +5,9 @@
 REVISION=a
 PROGRAMMER=dragon_isp
 PORT=usb
+VERBOSE=false
 
-while getopts ":c:P:r:h" opt; do
+while getopts ":c:P:r:hv" opt; do
 
 	case $opt in
 
@@ -29,8 +30,11 @@ while getopts ":c:P:r:h" opt; do
 		   echo "  -c [programmer] - avr programmer. Standard is dragon_isp."
 		   echo "  -P [port]       - programming port. Standard is usb."
 		   echo "  -r [revision]   - revision A or B. Standard is a."
+                   echo "  -v              - verbose mode."
 		   exit 0
 		   ;;   
+                v) VERBOSE=1
+                   ;;
 		\?) echo "Unknown Argument $OPT"
 			exit 1
 			;;
@@ -42,5 +46,11 @@ while getopts ":c:P:r:h" opt; do
 			
 done
 
-avrdude	 -pm8 -c$PROGRAMMER -P$PORT -b1200 -B50 -i50 -u -Uflash:w:bin/revision-$REVISION.hex:a -Ulfuse:w:0xc9:m -Uhfuse:w:0xd9:m
+PARAM="-pm8 -c$PROGRAMMER -P$PORT -b1200 -B50 -i50 -u -Uflash:w:bin/revision-$REVISION.hex:a -Ulfuse:w:0xc9:m -Uhfuse:w:0xd9:m "
+if [ $VERBOSE != 0 ]
+then
+ PARAM="$PARAM -vvv"
+fi
+
+avrdude $PARAM
 
